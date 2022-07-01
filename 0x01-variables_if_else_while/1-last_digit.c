0cb66740aa59962288d9712930375fc02b62d64c
obilala <abelzeki@yahoo.com>
2022-07-01 12:54:08 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("last digit of %d is ",n);
+printf("last digit of %d is ", n);
 if (n > 5)
 {
 printf("greater than 5");
@@ -23,9 +23,12 @@ if (n == 0)
 {
 printf("0");
 }
-if (n < 6 && n !=0)
+if (n < 6 && n != 0)
 {
 printf("less than 6 not 0");
 }
+
+printf("\n");
+
 return (0);
 }
