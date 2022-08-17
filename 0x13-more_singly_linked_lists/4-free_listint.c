59ae2a4e56a705ff99f6dc9c79ffc35c7e54f9ab
obilala <abelzeki@yahoo.com>
2022-08-17 14:50:52 +0300

    Create 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* free_listint - frees list
+* @head: head of linked list
+*/
+void free_listint(list_t *head)
+{
+list_t *tmp;
+list_t *next;
+tmp = head;
+while (tmp != NULL)
+{
+next = tmp->next;
+free(tmp->str);
+free(tmp);
+tmp = next;
+}
+}
