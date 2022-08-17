c71517f393eb2d302c58ba6f91e49e47acdd9949
obilala <abelzeki@yahoo.com>
2022-08-17 03:26:40 -0700

    com

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
 #ifndef LIST_H
 #define LIST_H
 #include <stddef.h>
-
 /**
 * struct list_s - singly linked list
 * @str: string - (malloc'ed string)
@@ -15,7 +14,6 @@ char *str;
 unsigned int len;
 struct list_s *next;
 } list_t;
-
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
