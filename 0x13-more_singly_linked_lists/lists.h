62d423dcdbdf993302dfb0b2c021ce00f5a52b2a
obilala <abelzeki@yahoo.com>
2022-08-18 15:14:58 +0300

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,36 +1,18 @@
-#ifndef _LISTS_
-#define _LISTS_
-
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
+#ifndef LIST_H
+#define LIST_H
+#include <stddef.h>
+/*
 * struct listint_s - singly linked list
-* @n: integer
+* @n: int - (malloc'ed string)
 * @next: points to the next node
-*
 * Description: singly linked list node structure
-*
 */
 typedef struct listint_s
 {
 int n;
 struct listint_s *next;
 } listint_t;
-
-/**
-* struct listp_s - singly linked list
-* @p: pointers of nodes
-* @next: points to the next nodes
-*
-* Description: singly linked list of pointers
-*/
-typedef struct listp_s
-{
-void *p;
-struct listp_s *next;
-} listp_t;
-
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -42,5 +24,4 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-
 #endif
